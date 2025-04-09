fb9fda019178935e4aeb9c11ee07156c929966ea
_[gen]_ <refflerg@roverkids.org>
2025-04-09 13:35:27 -0400

    Completed 1.2

---
+++ PlayerControl/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
